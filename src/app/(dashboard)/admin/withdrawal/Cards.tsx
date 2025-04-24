4a66f4c7a0532fffbd640f3b1d2b78c275d71a35
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2025-04-24 11:45:49 +0800

    pooosh

--- src/app/(dashboard)/admin/withdrawal/Cards.tsx
+++ src/app/(dashboard)/admin/withdrawal/Cards.tsx
@@ -44,7 +44,7 @@ export default function Cards() {
 
 
     <Card icon={<Wallet size={30} />} iconbg={' bg-yellow-500'} title={'Total Request Commission'} amount={`${total?.totalrequestcommission.toLocaleString()}`} subtitle={'User Total Request Commission'} text={''} loading={loading}/>
-    <Card icon={<Wallet size={30} />} iconbg={' bg-yellow-500'} title={'Total Miner Request'} amount={`${total?.totalrequestminecoin.toLocaleString()}`} subtitle={'User Total Miner Request'} text={''} loading={loading}/>
+    <Card icon={<Wallet size={30} />} iconbg={' bg-yellow-500'} title={'Chrono Package'} amount={`${total?.totalrequestminecoin.toLocaleString()}`} subtitle={'User Total Miner Request'} text={''} loading={loading}/>
    
 
 </div>
